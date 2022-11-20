56dabfa22a479f6991a11f6498c37124e742a23a
Ride_A_Pig <marthxia@gmail.com>
2022-11-20 16:54:51 -0700

    UI Update

--- GO/Assets/Resources/StartUI/Script_StartUI.cs
+++ GO/Assets/Resources/StartUI/Script_StartUI.cs
@@ -7,10 +7,15 @@ public class Script_StartUI : MonoBehaviour
 {
     [SerializeField] TMPro.TextMeshProUGUI TimeDisplay;
     [SerializeField] GameObject TurnOffPanel;
+    [SerializeField] GameObject BrowserPage;
+    [SerializeField] GameObject EmailPage;
+    [SerializeField] GameObject RemotePage;
     bool bTurnOffPanel = false;
     void Start()
     {
         TurnOffPanel.SetActive(false);
+        BrowserPage.SetActive(false);
+        EmailPage.SetActive(false);
     }
 
     void Update()
@@ -32,6 +37,41 @@ public class Script_StartUI : MonoBehaviour
     public void On_TurnOffPanelNo_Clicked()
     {
         On_TurnOffButton_Clicked();
+    }
+
+    public void On_Browser_Clicked()
+    {
+        BrowserPage.SetActive(true);
+    }
+
+    public void On_BrowserCloseButton_Clicked()
+    {
+        BrowserPage.SetActive(false);
+    }
+
+    public void On_Email_Clicked()
+    {
+        EmailPage.SetActive(true);
+    }
+
+    public void On_EmailPageCloseButton_Clicked()
+    {
+        EmailPage.SetActive(false);
+    }
+
+    public void On_EmailCard_Clicked()
+    {
+        print("Hello");
+    }
+
+    public void On_Remote_Clicked()
+    {
+        RemotePage.SetActive(true);
+    }
+
+    public void On_RemotePageBackButton_Clicked()
+    {
+        RemotePage.SetActive(false);
     }
 
 }
