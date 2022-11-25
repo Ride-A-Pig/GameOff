c1ad55c27c8c76269019930b483ed9c3ddec2eb8
Ride_A_Pig <marthxia@gmail.com>
2022-11-24 19:18:31 -0700

    Add click sfx

--- GO/Assets/Resources/StartUI/Script_StartUI.cs
+++ GO/Assets/Resources/StartUI/Script_StartUI.cs
@@ -10,6 +10,7 @@ public class Script_StartUI : MonoBehaviour
     [SerializeField] GameObject BrowserPage;
     [SerializeField] GameObject EmailPage;
     [SerializeField] GameObject RemotePage;
+    [SerializeField] AudioSource SFX;
     bool bTurnOffPanel = false;
     void Start()
     {
@@ -27,6 +28,7 @@ public class Script_StartUI : MonoBehaviour
     {
         TurnOffPanel.SetActive(!bTurnOffPanel);
         bTurnOffPanel = !bTurnOffPanel;
+        Play_SFX_Clicked();
     }
 
     public void On_TurnOffPanelYes_Clicked()
@@ -37,26 +39,36 @@ public class Script_StartUI : MonoBehaviour
     public void On_TurnOffPanelNo_Clicked()
     {
         On_TurnOffButton_Clicked();
+        Play_SFX_Clicked();
+
     }
 
     public void On_Browser_Clicked()
     {
         BrowserPage.SetActive(true);
+        Play_SFX_Clicked();
+
     }
 
     public void On_BrowserCloseButton_Clicked()
     {
         BrowserPage.SetActive(false);
+        Play_SFX_Clicked();
+
     }
 
     public void On_Email_Clicked()
     {
         EmailPage.SetActive(true);
+        Play_SFX_Clicked();
+
     }
 
     public void On_EmailPageCloseButton_Clicked()
     {
         EmailPage.SetActive(false);
+        Play_SFX_Clicked();
+
     }
 
     public void On_EmailCard_Clicked()
@@ -67,11 +79,20 @@ public class Script_StartUI : MonoBehaviour
     public void On_Remote_Clicked()
     {
         RemotePage.SetActive(true);
+        Play_SFX_Clicked();
+
     }
 
     public void On_RemotePageBackButton_Clicked()
     {
         RemotePage.SetActive(false);
+        Play_SFX_Clicked();
+
+    }
+
+    private void Play_SFX_Clicked()
+    {
+        SFX.Play();
     }
 
 }
